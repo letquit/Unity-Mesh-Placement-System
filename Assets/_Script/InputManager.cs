2e1f6fd44b5d50c4f675426281d5320563515c38
letquit <1446444589@qq.com>
2026-05-02 11:11:00 +0800

    Initial commit

---
+++ Assets/_Script/InputManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField] private LayerMask placementLayerMask;
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, placementLayerMask))
+        {
+            lastPosition = hit.point;
+        }
+
+        return lastPosition;
+    }
+}
---
+++ Assets/_Script/PlacementSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject mouseIndicator;
+    [SerializeField] private GameObject cellIndicator;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Grid grid;
+
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
